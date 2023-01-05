2a6a4460065b2df413fcd7e3cbc885e50d9b7252
JayLinXR <cooloojayoo@gmail.com>
2023-01-05 14:06:18 +1100

    Fix - Divide Section

--- layouts/DivideSection/DivideSection.tsx
+++ layouts/DivideSection/DivideSection.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const DivideSection: FC = () => {
   return (
-    <section className="w-full h-[62px] md:h-12 py-2.5 px-8 md:py-[11px] md:px-0 bg-celadon flex items-center justify-center text-center">
+    <section className="w-full h-[62px] md:h-12 py-2.5 px-4 md:py-[11px] md:px-0 bg-celadon flex items-center justify-center text-center">
       <span className="text-dark-green text-sm md:text-[18px] font-semibold">
         If you&apos;re in crisis or experiencing suicidal thoughts call 000 or
         Lifeline 13 11 14
